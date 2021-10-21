dac0fb5b31a2cb2aea33797757eee40900438cc5
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-10-21 16:38:15 +0200

    Updated latest suggestions

--- src/article.py
+++ src/article.py
@@ -3,12 +3,12 @@ import itertools
 
 class Article:
     """
-    Class which contains all the data from an article
+    Class which contains all the data from an article.
 
     Attributes
     ----------
     id_iter : itertools
-        Iterator used to obtain an unique id
+        Iterator used to obtain an unique id.
 
     """
 
@@ -20,70 +20,78 @@ class Article:
 
         Parameters
         ------
-        headline : string, default=''
-        author : string, default=''
-        content : string, default=''
-        topic : string, default=''
-        source : string, default=''
-
+        headline : str, default=''
+            Article's headline.
+        author : str, default=''
+            Person who wrote the article.
+        content : str, default=''
+            Content of the article.
+        topic : str, default=''
+            Subject of the article.
+        source : str, default=''
+            Where the article comes from.
         Raises
         ------
         ValueError
-            Headline or author is not valid
+            Headline or author is not valid.
 
         """
-        self._id = next(Article.id_iter)
-        self._content = content
-        self._topic = topic
-        self._source = source
+        if not headline and not content:
+            raise ValueError('An article must include a headline or content')
+
         if(len(headline) < globals.HEADLINE_MAX_SIZE):
             self._headline = headline
         else:
             raise ValueError('Headline cannot be larger than ' + str(globals.HEADLINE_MAX_SIZE))
         
-        if(globals.AUTHOR_MAX_LENGHT > len(author) >= globals.INVALID):
+        if(globals.AUTHOR_MAX_LENGHT > len(author)):
             self._author = author
         else:
             raise ValueError('Author cannot be larger than ' + str(globals.AUTHOR_MAX_LENGHT))
 
+        self._id = next(Article.id_iter)
+        self._content = content
+        self._topic = topic
+        self._source = source
+
     @property
     def author(self):
         """
-        Returns the author of the article
+        Returns the author of the article.
         """
         return self._author
     
     @property
     def content(self):
         """
-        Returns the content of the article
+        Returns the content of the article.
         """
         return self._content
 
     @property
     def source(self):
         """
-        Returns the source of the article
+        Returns the source of the article.
         """
         return self._source
 
     @property
     def topic(self):
         """
-        Returns the topic of the article
+        Returns the topic of the article.
         """
         return self._topic
 
     @property
     def headline(self):
         """
-        Returns the headline of the article
+        Returns the headline of the article.
         """
         return self._headline
     
     @property
     def id(self):
         """
-        Returns the id of the article
+        Returns the id of the article.
         """
         return self._id
