6ab6b11273bb5c6bf330f7c2e9d412e35fca7df4
arguellesm <m.arguelles28@gmail.com>
2022-02-05 00:29:51 +0100

    uses pytruth assertions

--- test/article_test.py
+++ test/article_test.py
@@ -1,4 +1,5 @@
 import pytest
+from truth.truth import AssertThat
 from src.article import Article
 from src.article import ArticleValueError, CantLoadContentModelError, ContentModelError    
 
@@ -16,17 +17,17 @@ def test_can_create_object_only_content():
 
 
 def test_cant_create_invalid_object_no_headline_nor_content():
-    with pytest.raises(ArticleValueError):
+    with AssertThat(ArticleValueError).IsRaised():
         Article(author='Jane Doe', topic='health', source='NYT')
 
 
 def test_cant_create_invalid_object_hempty_headline_no_content():
-    with pytest.raises(ArticleValueError):
+    with AssertThat(ArticleValueError).IsRaised():
         Article(headline='')
 
 
 def test_cant_create_invalid_object_hempty_content_no_headline():
-    with pytest.raises(ArticleValueError):
+    with AssertThat(ArticleValueError).IsRaised():
         Article(content='')
 
 
@@ -35,27 +36,27 @@ def test_has_headline_or_content():
 
 
 def test_predict_returns_three_values():
-    assert len(article.predict()) == 3
+    AssertThat(len(article.predict())).IsEqualTo(3)
 
 
 def test_content_between_0_and_1():
-    assert 0 <= article.rate_content() <= 1
+    AssertThat(0 <= article.rate_content() <= 1).IsTruthy()
 
 
 def test_author_score_is_0():
-    assert article.rate_author() == 0
+    AssertThat(article.rate_author()).IsEqualTo(0)
 
 
 def test_source_score_is_0():
-    assert article.rate_source() == 0
+    AssertThat(article.rate_source()).IsEqualTo(0)
 
 
 def test_dummy_model_raises_cant_load_content_model_error_exception():
-    with pytest.raises(CantLoadContentModelError):
+    with AssertThat(CantLoadContentModelError).IsRaised():
         article.rate_content(debug=True)
 
 
 def test_corrupted_article_raises_content_model_error_exception():
     article.content = 10
-    with pytest.raises(ContentModelError):
+    with AssertThat(ContentModelError).IsRaised():
         article.rate_content()
